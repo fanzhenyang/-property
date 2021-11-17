26d5b41928f3c70b50f3237ed8816791d39ba4b2
fanzhenyang <894578483@qq.com>
2021-11-17 17:37:23 +0800

    开发loading

--- src/hooks/loading.ts
+++ src/hooks/loading.ts
@@ -1,18 +1,18 @@
-import { Component, createApp } from 'vue'
-export default function useLoading(Comp: Component): any {
+import { createApp } from 'vue'
+export default function useLoading(Comp: any): any {
   return {
-    mounted(el: HTMLElement, binding: any) {
+    mounted(el: any, binding: any) {
       const app = createApp(Comp)
       const instance = app.mount(document.createElement('div'))
       const name = Comp?.name
-      // if (!el[name]) {
-      //   el[name] = {}
-      // }
-      // el[name].instance = instance
-      console.log('%c 🌽 name: ', 'font-size:20px;background-color: #33A5FF;color:#fff;', name)
+      if (!el[name]) {
+        el[name] = {}
+      }
+      el[name].instance = instance
+      // const title = binding.arg
       console.log('%c 🥘 instance: ', 'font-size:20px;background-color: #FCA650;color:#fff;', instance)
       console.log('%c 🥚 binding: ', 'font-size:20px;background-color: #FFDD4D;color:#fff;', binding)
-      console.log('%c 🍠 el: ', 'font-size:20px;background-color: #93C0A4;color:#fff;', el)
+      console.log('%c 🍠 el: ', 'font-size:20px;background-color: #93C0A4;color:#fff;', el[name])
     }
   }
 }
